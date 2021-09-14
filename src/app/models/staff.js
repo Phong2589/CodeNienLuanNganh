8bc381efa468fcb35ec41349c32043b88844ee91
NienLuanNganhCT466 <nienluannganhct466@gmail.com>
2021-09-14 10:26:11 +0700

    mongodb atlas

--- src/app/models/staff.js
+++ src/app/models/staff.js
@@ -4,6 +4,8 @@ const Schema = mongoose.Schema;
 const staff = new Schema({
     user: { type: String},
     password: { type: String},
+  },{
+    timestamps: true,
   });
 
 module.exports = mongoose.model('staff', staff);
--- src/config/db/index.js
+++ src/config/db/index.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 async function connect(){
     try{
-        await mongoose.connect('mongodb://localhost:27017/pqshop');
+        //await mongoose.connect('mongodb://localhost:27017/pqshop');
+        await mongoose.connect('mongodb+srv://phong:phongpqshop@pqshop.ibooi.mongodb.net/pqshop?retryWrites=true&w=majority');
         console.log('connect sucessfully!!!')
     }
     catch(error)
