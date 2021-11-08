a760257f89185be98df05053ef00dbf9bcf03a9e
NienLuanNganhCT466 <nienluannganhct466@gmail.com>
2021-11-08 12:51:36 +0700

    built upload img

--- src/routes/customer.js
+++ src/routes/customer.js
@@ -3,7 +3,7 @@ const path = require('path');
 const router = express.Router();
 const passport = require('passport');
 const multer  = require('multer')
-console.log(path.join(__dirname, '../resources/views'))
+console.log(path.join(__dirname, '../src/public/img/'))
 const upload = multer({ dest: path.join(__dirname, '../src/public/img/')})
 const customerController = require('../app/controllers/customerController');
 
