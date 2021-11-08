3f5df4a094c8cce99b16e44f9f17bbe091a6fe7f
NienLuanNganhCT466 <nienluannganhct466@gmail.com>
2021-11-08 10:54:01 +0700

    built git ignore

--- src/util/facebook.js
+++ src/util/facebook.js
@@ -10,13 +10,3 @@ passport.deserializeUser(function(user, done) {
 });
 
  
-// passport.use(new FacebookStrategy({
-//   clientID: "583964342925090",
-//   clientSecret: "698e6747f90aa3ae71cff81e81bdbdc1",
-//   callbackURL: "http://localhost:8080/registerFacebook/callback",
-//   // callbackURL: "https://pqshop.herokuapp.com/registerFacebook/callback",
-// },
-// function(accessToken, refreshToken, profile, done) {
-//   return done(null, profile);
-// }
-// ));
